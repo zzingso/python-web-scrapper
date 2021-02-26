356df06596e6c7005f5b7e03e7d9918d2dce25ac
zzingso <ravie90@gmail.com>
2021-02-26 07:12:50 +0000

    Scrapping WIP

--- so.py
+++ so.py
@@ -18,23 +18,28 @@ def extract_job(html):
 
     company, location = html.find("h3").find_all("span", recursive=False)
 
-    print(location.get_text(strip=True))
-    print(company.get_text(strip=True))
-    print(title)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True).strip(" \r").strip("\n")
+    job_id = html["data-jobid"]
 
-    return {"title": title, "company": company, "location": location}
+    return {
+        "title": title,
+        "company": company,
+        "location": location,
+        "link": f"https://stackoverflow.com/jobs/{job_id}"
+    }
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-      print(f"Scrapping SO: Page {page+1}")
-      result = requests.get(f"{URL}&pg={page+1}")
-      soup = BeautifulSoup(result.text, "html.parser")
-      job_results = soup.find_all("div", {"class": "-job"})
-      
-      for job in job_results:
-        jobs.append(extract_job(job))
+        print(f"Scrapping SO: Page {page+1}")
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        job_results = soup.find_all("div", {"class": "-job"})
+
+        for job in job_results:
+            jobs.append(extract_job(job))
 
     return jobs
 
