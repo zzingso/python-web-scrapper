41701f62fc9f6869b378d82c9105e4f5fddd5370
zzingso <ravie90@gmail.com>
2021-03-02 04:24:39 +0000

    add package "Flask"

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from flask import Flask
 from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
 from save import save_to_file
 
-indeed_jobs = get_indeed_jobs()
-so_jobs = get_so_jobs()
+# indeed_jobs = get_indeed_jobs()
+# so_jobs = get_so_jobs()
 
-jobs = indeed_jobs + so_jobs
-save_to_file(jobs)
+# jobs = indeed_jobs + so_jobs
+# save_to_file(jobs)
+
+app = Flask("WebScrapping")
+
+
+@app.route("/")
+def home():
+    return "Hello!"
+
+
+app.run(host="0.0.0.0")
