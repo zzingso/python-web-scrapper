0608c02803849684a7558e86c248d881c5b12f4d
zzingso <ravie90@gmail.com>
2021-03-02 07:16:57 +0000

    Flask SO Scrapping

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
 from save import save_to_file
@@ -9,12 +9,33 @@ from save import save_to_file
 # jobs = indeed_jobs + so_jobs
 # save_to_file(jobs)
 
+db = {}
+
 app = Flask("WebScrapping")
 
 
 @app.route("/")
 def home():
-    return "Hello!"
+    return render_template("home.html")
 
 
-app.run(host="0.0.0.0")
+@app.route("/report")
+def report():
+    word = request.args.get("word")
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_so_jobs(word)
+            db[word] = jobs
+        return render_template("report.html",
+                               searchingBy=word,
+                               resultsNumber=len(jobs),
+                               jobs=jobs)
+    else:
+        return redirect("/")
+
+
+app.run(host="0.0.0.0", port=8080)
--- so.py
+++ so.py
@@ -1,11 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://stackoverflow.com/jobs?q=python"
 
-
-def get_last_page():
-    result = requests.get(URL)
+def get_last_page(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
     last_page = pages[-2].get_text(strip=True)
@@ -30,11 +28,11 @@ def extract_job(html):
     }
 
 
-def extract_jobs(last_page):
+def extract_jobs(last_page, url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping SO: Page {page+1}")
-        result = requests.get(f"{URL}&pg={page+1}")
+        result = requests.get(f"{url}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         job_results = soup.find_all("div", {"class": "-job"})
 
@@ -44,8 +42,9 @@ def extract_jobs(last_page):
     return jobs
 
 
-def get_jobs():
-    last_page = get_last_page()
-    jobs = extract_jobs(last_page)
+def get_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
 
     return jobs
