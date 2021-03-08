f3107fc06b156eb959805c8aef7a3c34967958f5
zzingso <ravie90@gmail.com>
2021-03-08 02:04:15 +0000

    csv export

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
 from save import save_to_file
@@ -35,6 +35,28 @@ def report():
                                resultsNumber=len(jobs),
                                jobs=jobs)
     else:
+        return redirect("/")
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get("word")
+        if not word:
+            raise Exception()
+        word = word.lower()
+
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+
+        save_to_file(jobs)
+
+        return send_file("jobs.csv",
+                         mimetype="text/csv",
+                         attachment_filename="jobs.csv",
+                         as_attachment=True)
+    except:
         return redirect("/")
 
 
